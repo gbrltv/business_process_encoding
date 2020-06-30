a5aa4b5101995e06df9c2fe46fb4a63173dc65df
gbrltv <gabrielmrqstvrs@gmail.com>
2020-06-30 22:39:30 +0200

    Adding alignment

---
+++ compute_encoding/alignment.py
@@ -0,0 +1,58 @@
+import os
+import time
+import pandas as pd
+from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py.algo.discovery.inductive import algorithm as inductive_miner
+from pm4py.algo.conformance.alignments import algorithm as alignments
+from pm4py.algo.conformance.alignments import versions
+from utils import read_log
+from utils import sort_alphanumeric
+from utils import retrieve_traces
+
+
+def compute_alignments(alignments):
+    cost, visited_states, queued_states, traversed_arcs, fitness = [], [], [], [], []
+    for alignment in trace_alignments:
+        cost.append(alignment['cost'])
+        visited_states.append(alignment['visited_states'])
+        queued_states.append(alignment['queued_states'])
+        traversed_arcs.append(alignment['traversed_arcs'])
+        fitness.append(alignment['fitness'])
+
+    return [cost, visited_states, queued_states, traversed_arcs, fitness]
+
+path = './event_logs'
+save_path = './encoding_results/alignment'
+os.makedirs(save_path, exist_ok=True)
+for file in sort_alphanumeric(os.listdir(path)):
+    # read event log and import case id and labels
+    ids, traces, y = retrieve_traces(read_log(path, file))
+
+    # import log
+    file_xes = file.split('.csv')[0]
+    log = xes_importer.apply(f'{path}_xes/{file_xes}.xes')
+
+    start_time = time.time()
+
+    # generate process model
+    net, initial_marking, final_marking = inductive_miner.apply(log)
+
+    # compute alignments
+    trace_alignments = alignments.apply_log(log, net, initial_marking, final_marking, variant=versions.dijkstra_no_heuristics)
+
+    end_time = time.time() - start_time
+
+    final_alignments = compute_alignments(trace_alignments)
+
+
+    # saving
+    out_df = pd.DataFrame()
+    out_df['cost'] = final_alignments[0]
+    out_df['visited_states'] = final_alignments[1]
+    out_df['queued_states'] = final_alignments[2]
+    out_df['traversed_arcs'] = final_alignments[3]
+    out_df['fitness'] = final_alignments[4]
+    out_df['case'] = ids
+    out_df['time'] = end_time
+    out_df['label'] = y
+    out_df.to_csv(f'{save_path}/{file}', index=False)
