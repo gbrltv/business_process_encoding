00a229f4613fe61a6ce9c74dca4c892ef61cf8a0
gbrltv <gabrielmrqstvrs@gmail.com>
2020-06-30 23:15:01 +0200

    Hash Vectorizer encoding

--- compute_encoding/utils/extract_corpus.py
+++ compute_encoding/utils/extract_corpus.py
@@ -1,12 +1,27 @@
-def extract_corpus(df):
+import pandas as pd
+
+def extract_corpus(df: pd.DataFrame):
     """
     Creates corpus for model training
+
+    Parameters
+    -----------------------
+    df: pd.DataFrame,
+        Dataframe containing the event log
+    Returns
+    -----------------------
+    ids: List,
+        List of case ids
+    traces: List,
+        List of traces
+    y: List,
+        List of case labels
     """
-    cases, y, ids = [], [], []
+    traces, y, ids = [], [], []
     for group in df.groupby('case'):
         events = list(group[1].activity_name)
-        cases.append(' '.join(x for x in events))
+        traces.append(' '.join(x for x in events))
         y.append(list(group[1].label)[0])
         ids.append(list(group[1].case)[0])
 
-    return ids, cases, y
+    return ids, traces, y
